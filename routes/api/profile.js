e45e68be2351e887d5838845baabe2b14d10ed3d
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-05-03 15:32:51 +0100

    delete profile&user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -148,4 +148,22 @@ router.get('/user/:user_id', async (req, res) => {
     }
 })
 
+//@route  DELETE api/profile
+//@desc   Delete profile, user & post
+//@access Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        //@to-do - remove users posts
+
+        //Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        //Remove User
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User deleted' })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
