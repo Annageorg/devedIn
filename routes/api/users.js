d7a066e3892521d5c3e3b60e9be7dfdc8a689103
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-05-03 10:50:11 +0100

    jwt token set up

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -30,11 +32,11 @@ router.post(
 
         try {
             //See if user exists
-            let user = await User.findOne({ email })
+            let user = await User.findOne({ email });
             if (user) {
-               return  res
-                .status(400)
-                .json({ errors: [{ msg: 'User already exists' }] });
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
             }
             //Get users gravatar
             const avatar = gravatar.url(
@@ -60,15 +62,26 @@ router.post(
             await user.save();
 
             //Return the jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User registered');
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            )
+
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
         }
-
-
-
     }
 );
 
