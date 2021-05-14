10e655d35260f378df21a1bb038487c135142025
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-05-14 23:20:19 +0100

    edit profile

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -1,10 +1,15 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createProfile } from '../../actions/profile';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
-const CreateProfile = ({ createProfile, history }) => {
+const EditProfile = ({
+    profile: { profile, loading },
+    createProfile,
+    getCurrentProfile,
+    history
+}) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -22,6 +27,25 @@ const CreateProfile = ({ createProfile, history }) => {
 
     const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
+    useEffect(() => {
+        getCurrentProfile();
+
+        setFormData({
+            company: loading || !profile.company ? '' : profile.company,
+            website: loading || !profile.website ? '' : profile.website,
+            location: loading || !profile.location ? '' : profile.location,
+            status: loading || !profile.status ? '' : profile.status,
+            skills: loading || !profile.skills ? '' : profile.skills,
+            githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
+            bio: loading || !profile.bio ? '' : profile.bio,
+            twitter: loading || !profile.social ? '' : profile.social.twitter,
+            facebook: loading || !profile.social ? '' : profile.social.facebook,
+            linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+            youtube: loading || !profile.social ? '' : profile.social.youtube,
+            instagram: loading || !profile.social ? '' : profile.social.instagram
+        })
+    }, [loading])
+
     const {
         company,
         website,
@@ -37,11 +61,11 @@ const CreateProfile = ({ createProfile, history }) => {
         linkedin
     } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
     const onSubmit = e => {
         e.preventDefault();
-        createProfile(formData, history)
+        createProfile(formData, history, true)
     }
 
     return (
@@ -56,9 +80,9 @@ const CreateProfile = ({ createProfile, history }) => {
             <small>* = required field</small>
             <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                    <select 
-                        name="status" 
-                        value={status} 
+                    <select
+                        name="status"
+                        value={status}
                         onChange={(e) => onChange(e)}
                     >
                         <option value="0">* Select Professional Status</option>
@@ -76,11 +100,11 @@ const CreateProfile = ({ createProfile, history }) => {
                     </small>
                 </div>
                 <div className="form-group">
-                    <input 
-                        type="text" 
-                        placeholder="Company" 
-                        name="company" 
-                        value={company} 
+                    <input
+                        type="text"
+                        placeholder="Company"
+                        name="company"
+                        value={company}
                         onChange={(e) => onChange(e)}
                     />
                     <small className="form-text">
@@ -88,36 +112,36 @@ const CreateProfile = ({ createProfile, history }) => {
                     </small>
                 </div>
                 <div className="form-group">
-                    <input 
-                        type="text" 
-                        placeholder="Website" 
+                    <input
+                        type="text"
+                        placeholder="Website"
                         name="website"
                         value={website}
-                        onChange={(e) => onChange(e)} 
+                        onChange={(e) => onChange(e)}
                     />
                     <small className="form-text">
                         Could be your own or a company website
                     </small>
                 </div>
                 <div className="form-group">
-                    <input 
-                        type="text" 
-                        placeholder="Location" 
-                        name="location" 
+                    <input
+                        type="text"
+                        placeholder="Location"
+                        name="location"
                         value={location}
-                        onChange={(e) => onChange(e)} 
+                        onChange={(e) => onChange(e)}
                     />
                     <small className="form-text">
                         City & state suggested (eg. Boston, MA)
                     </small>
                 </div>
                 <div className="form-group">
-                    <input 
-                        type="text" 
-                        placeholder="* Skills" 
-                        name="skills" 
+                    <input
+                        type="text"
+                        placeholder="* Skills"
+                        name="skills"
                         value={skills}
-                        onChange={(e) => onChange(e)} 
+                        onChange={(e) => onChange(e)}
                     />
                     <small className="form-text">
                         Please use comma separated values
@@ -130,15 +154,15 @@ const CreateProfile = ({ createProfile, history }) => {
                         placeholder="Github Username"
                         name="githubusername"
                         value={githubusername}
-                        onChange={(e) => onChange(e)} 
+                        onChange={(e) => onChange(e)}
                     />
                     <small className="form-text">
                         If you want your latest repos and a Github link, include your username
                     </small>
                 </div>
                 <div className="form-group">
-                    <textarea 
-                        placeholder="A short bio of yourself" 
+                    <textarea
+                        placeholder="A short bio of yourself"
                         name="bio"
                         value={bio}
                         onChange={(e) => onChange(e)}
@@ -155,50 +179,50 @@ const CreateProfile = ({ createProfile, history }) => {
                 {displaySocialInputs && <Fragment>
                     <div className="form-group social-input">
                         <i className="fab fa-twitter fa-2x"></i>
-                        <input 
-                            type="text" 
-                            placeholder="Twitter URL" 
+                        <input
+                            type="text"
+                            placeholder="Twitter URL"
                             name="twitter"
                             value={twitter}
-                            onChange={(e) => onChange(e)} 
+                            onChange={(e) => onChange(e)}
                         />
                     </div>
                     <div className="form-group social-input">
                         <i className="fab fa-facebook fa-2x"></i>
-                        <input 
-                            type="text" 
-                            placeholder="Facebook URL" 
-                            name="facebook" 
+                        <input
+                            type="text"
+                            placeholder="Facebook URL"
+                            name="facebook"
                             value={facebook}
                             onChange={(e) => onChange(e)}
                         />
                     </div>
                     <div className="form-group social-input">
                         <i className="fab fa-youtube fa-2x"></i>
-                        <input 
-                            type="text" 
-                            placeholder="YouTube URL" 
-                            name="youtube" 
+                        <input
+                            type="text"
+                            placeholder="YouTube URL"
+                            name="youtube"
                             value={youtube}
                             onChange={(e) => onChange(e)}
                         />
                     </div>
                     <div className="form-group social-input">
                         <i className="fab fa-linkedin fa-2x"></i>
-                        <input 
-                            type="text" 
-                            placeholder="Linkedin URL" 
-                            name="linkedin" 
+                        <input
+                            type="text"
+                            placeholder="Linkedin URL"
+                            name="linkedin"
                             value={linkedin}
                             onChange={(e) => onChange(e)}
                         />
                     </div>
                     <div className="form-group social-input">
                         <i className="fab fa-instagram fa-2x"></i>
-                        <input 
-                            type="text" 
-                            placeholder="Instagram URL" 
-                            name="instagram" 
+                        <input
+                            type="text"
+                            placeholder="Instagram URL"
+                            name="instagram"
                             value={instagram}
                             onChange={(e) => onChange(e)}
                         />
@@ -212,8 +236,17 @@ const CreateProfile = ({ createProfile, history }) => {
     )
 }
 
-CreateProfile.propTypes = {
-    createProfile: PropTypes.func.isRequired
+EditProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired,
+    getCurrentProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired
 }
 
-export default connect(null, { createProfile })(withRouter(CreateProfile));
+const mapStateToProps = state => ({
+    profile: state.profile
+})
+
+export default connect(
+    mapStateToProps,
+    { createProfile, getCurrentProfile }
+)(withRouter(EditProfile));
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -78,7 +78,11 @@ router.post(
         if (status) profileFields.status = status;
         if (githubusername) profileFields.githubusername = githubusername;
         if (skills) {
-            profileFields.skills = skills.split(',').map(skill => skill.trim());
+            if (Array.isArray(skills)){
+                profileFields.skills = skills;
+            } else {
+                profileFields.skills = skills.split(',').map(skill => skill.trim());
+            }    
         }
 
         //Build social object
