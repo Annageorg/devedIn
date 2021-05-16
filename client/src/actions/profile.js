ee3d83d5e38bc397a1d09267a74a3d8e28a1ae70
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-05-16 20:50:35 +0100

    profile profile top and about comps

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -49,7 +49,7 @@ export const getProfiles = () => async dispatch => {
 }
 
 //Get profile by ID
-export const getProfile = (userId) => async dispatch => {
+export const getProfileById = (userId) => async dispatch => {
     try {
         const res = await axios.get(`/api/profile/user/${userId}`);
 
